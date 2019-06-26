52f36c75480493bb0d0e620da0dc876c5beb6241
Ramon Silva <silva018-mg@yahoo.com.br>
2019-06-25 23:23:39 -0300

    Adding graphql support

--- start/routes.js
+++ start/routes.js
@@ -1,22 +1,17 @@
-'use strict'
-
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
+const GraphQLServer = use('GraphQLServer')
 
-Route.get('/', async () => {
-  return { 'result': 'MapOS API' }
+Route.post('/', (context) => {
+  return GraphQLServer.handle(context)
 })
 
+// Route.get('/graphiql', (context) => {
+//   return GraphQLServer.handleUI(context)
+// })
+
+// or add options (example)
+Route.get("/graphiql", (context) => {
+  return GraphQLServer.handleUI(context, {
+    passHeader: `'Authorization': '${context.request.header("Authorization")}'`
+  })
+})
