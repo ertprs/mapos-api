1cf73da9e4a08a855d98abf76c69bf79c097be5a
Ramon Silva <silva018-mg@yahoo.com.br>
2019-08-04 13:18:17 -0300

    Adding graphql depth limit

--- config/graphql.js
+++ config/graphql.js
@@ -1,11 +1,15 @@
 'use strict'
 
 const { join } = require('path')
+const depthLimit = require('graphql-depth-limit')
+const Env = use('Env')
+
 
 module.exports = {
   options: {
     debug: false,
     endpointURL: '/',
+    validationRules: [depthLimit( Env.get('APP_DEPTH_LIMIT', 3) )]
   },
 
   schema: join(__dirname, '../app/Schema'),
